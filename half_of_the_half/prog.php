4a61f21406a586d35b380f6f318c4b505dd39350
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-10-09 21:55:08 +0200

    Solved Half of the Half

---
+++ half_of_the_half/prog.php
@@ -0,0 +1,22 @@
+<?php
+
+$lines = file('php://stdin');
+$maxLines = $lines[0];
+for($i = 1; $i <= $maxLines; $i++){
+  $halfSentence = substr($lines[$i], 0, (strlen($lines[$i])-1) / 2);
+$letters = str_split($halfSentence);
+$even = '';
+$odd = '';
+foreach ($letters as $k => $v) {
+    if ($k % 2 == 0) {
+        $even.= $v;
+    }
+    else {
+        $odd.= $v;
+    }
+}
+
+  fwrite(STDOUT, $even . "\n");
+} 
+
+
