ea1902075819cc2a2a02f43e2f34793b7f7657a1
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-05-28 12:32:22 +0200

    Feynman total squares

---
+++ samer08f/Feynman.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        int no;
+        cin>>no;
+        if(no==0)
+            break;
+        int total=0;    
+        while(no>0)
+        {
+            total = total + (no*no);
+            no--;
+        }
+        cout<<total<<"\n";
+    }
+    return 0;
+}
